374d4cc81241ce64e17596e42d471b11e73dc582
Wasif Khan <waasif.khaan@gmail.com>
2018-08-14 22:39:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ def self.find_by_name(name)
 end
 
 def self.find_by(input)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = #{input}"
+    sql = "SELECT * FROM #{self.table_name} WHERE id = #{input}"
     DB[:conn].execute(sql)
 end 
 end
