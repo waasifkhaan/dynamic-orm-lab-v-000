d5bb8917bf6b7219e80ea05be75d3b7e09f3c1dc
Wasif Khan <waasif.khaan@gmail.com>
2018-08-14 22:40:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,5 +11,9 @@ def self.find_by_name(name)
     DB[:conn].execute(sql,name)
 end
 
+def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ? "
+    DB[:conn].execute(sql,name)
+end
 
 end
