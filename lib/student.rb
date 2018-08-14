3e2cdedffff3545014be9728af2860377a174f9f
Wasif Khan <waasif.khaan@gmail.com>
2018-08-14 22:30:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ def self.find_by_name(name)
 end
 
 def self.find_by(input)
-  sql = "SELECT * FROM #{self.table_name} WHERE  "}
+  sql = "SELECT * FROM #{self.table_name} WHERE name = ? || WHERE id = ? || WHERE grade = ? "}
     DB[:conn].execute(sql)
 end 
 end
