bf67ed0141cd9460982fcb846e0ba023c6050814
Wasif Khan <waasif.khaan@gmail.com>
2018-08-14 22:23:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ def self.find_by_name(name)
 end
 
 def self.find_by(input)
-  sql = "SELECT * FROM #{self.table_name} WHERE #{column_names} = #{input} "
+  sql = "SELECT * FROM #{self.table_name} WHERE #{self.column_names} = #{input} "
     DB[:conn].execute(sql)
 end 
 end
