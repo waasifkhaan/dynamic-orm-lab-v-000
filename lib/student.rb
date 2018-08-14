9c0870e7c2daed526e0738175e83731ecd0d824f
Wasif Khan <waasif.khaan@gmail.com>
2018-08-14 23:04:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ def self.find_by_name(name)
 end
 
 def self.find_by(arg)
-    arg.each do |key, value|
+    arg.collect do |key, value|
       sql = "SELECT * FROM #{self.table_name} WHERE #{key} = #{value} "
       DB[:conn].execute(sql)
     end 
